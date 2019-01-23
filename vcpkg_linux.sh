3776486d04aae6db9c8439aa854abe631a9d881c
Tanguy Fautre <GPSnoopy@users.noreply.github.com>
2019-01-23 15:24:41 +0000

    Switching back to vcpkg master branch (#37)
    * Switching back to vcpkg master branch
    
    * Update vcpkg_linux.sh

--- vcpkg_linux.sh
+++ vcpkg_linux.sh
@@ -3,9 +3,8 @@ set -e
 
 mkdir -p build
 cd build
-git clone https://github.com/philjdf/vcpkg.git vcpkg.linux
+git clone https://github.com/Microsoft/vcpkg.git vcpkg.linux
 cd vcpkg.linux
-git checkout ArrowUpdateLinux
 ./bootstrap-vcpkg.sh
 
 ./vcpkg install arrow:x64-linux
