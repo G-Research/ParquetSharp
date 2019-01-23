e6454580787663c8cb7fd04aba9015c09a7625ff
Tanguy Fautre <tanguy@fautre.com>
2019-01-23 18:55:18 +0000

    Added support for Decimal in RowOriented API (tuples are not currently supported, due to the need for an attribute)

---
+++ csharp/RowOriented/ParquetDecimalScale.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ParquetSharp.RowOriented
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public sealed class ParquetDecimalScaleAttribute : Attribute
+    {
+        public ParquetDecimalScaleAttribute(int scale)
+        {
+            Scale = scale;
+        }
+
+        public readonly int Scale;
+    }
+}
