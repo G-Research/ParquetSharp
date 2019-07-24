de1821d86bc089d36a185e66eb1873e98d8b0f36
Tanguy Fautre <tanguy@fautre.com>
2019-07-24 19:14:33 +0100

    Point back at vcpkg master, now that Arrow 0.14.1 port has been merged. Bumped version to 2.0.1-beta1

--- vcpkg_windows.bat
+++ vcpkg_windows.bat
@@ -1,8 +1,7 @@
 mkdir build
 cd build || goto :error
-git clone https://github.com/philjdf/vcpkg.git vcpkg.windows || goto :error
+git clone https://github.com/microsoft/vcpkg.git vcpkg.windows || goto :error
 cd vcpkg.windows || goto :error
-git checkout Arrow014  || goto :error
 call bootstrap-vcpkg.bat || goto :error
 
 vcpkg.exe install arrow:x64-windows-static || goto :error
