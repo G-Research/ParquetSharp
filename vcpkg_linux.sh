adc5080e381cc24eb242dbcb396f94e843b4832e
Tanguy Fautre <GPSnoopy@users.noreply.github.com>
2019-07-25 10:40:29 +0100

    Point back at vcpkg master, now that Arrow 0.14.1 port has been merged. (#62)
    Bumped version to 2.0.1-beta1

--- vcpkg_linux.sh
+++ vcpkg_linux.sh
@@ -3,9 +3,8 @@ set -e
 
 mkdir -p build
 cd build
-git clone https://github.com/philjdf/vcpkg.git vcpkg.linux
+git clone https://github.com/microsoft/vcpkg.git vcpkg.linux
 cd vcpkg.linux
-git checkout Arrow014
 ./bootstrap-vcpkg.sh
 
 ./vcpkg install arrow:x64-linux
