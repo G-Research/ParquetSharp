e8e516ff85586c568c838136797933e9dd1962c3
Tanguy Fautre <GPSnoopy@users.noreply.github.com>
2020-01-02 15:27:20 +0000

    Update vcpkg_windows.bat

--- vcpkg_windows.bat
+++ vcpkg_windows.bat
@@ -2,7 +2,7 @@ mkdir build
 cd build || goto :error
 git clone https://github.com/microsoft/vcpkg.git vcpkg.windows || goto :error
 cd vcpkg.windows || goto :error
-git checkout 2019.10 || goto :error
+git checkout 2019.11 || goto :error
 call bootstrap-vcpkg.bat || goto :error
 
 vcpkg.exe install arrow:x64-windows-static || goto :error
