2e6f57d00d9aeba36dfb3b65ffec61c8d130dd6f
Phil Felton <39185762+philjdf@users.noreply.github.com>
2020-01-31 16:13:30 +0000

    Initial upgrade to Arrow 0.16 (#93)
    * Upgrading to Arrow-0.16.
    Fixing IO API changes.
    
    * Added support for BZip2 compression.
    
    * Fixed unit test on Windows.
    
    * Fixed OpenSSL linking

--- csharp.test/TestParquetFileReader.cs
+++ csharp.test/TestParquetFileReader.cs
@@ -17,9 +17,9 @@ namespace ParquetSharp.Test
             var isLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
 
             Assert.AreEqual(
-                (isLinux ? "N7parquet16ParquetExceptionE" : "class parquet::ParquetException") +
-                " (message: 'Arrow error: IOError: Failed to open local file 'non_existent.parquet', error: " +
-                (isLinux ? "No such file or directory" : "The system cannot find the file specified." + Environment.NewLine) +
+                (isLinux ? "N7parquet16ParquetExceptionE" : "class parquet::ParquetStatusException") +
+                " (message: 'IOError: Failed to open local file 'non_existent.parquet'. Detail: " +
+                (isLinux ? "No such file or directory" : "[Windows error 2] The system cannot find the file specified." + Environment.NewLine) +
                 "')",
                 exception.Message);
         }
