2907c466f1366daf01156895ccabd9e703d06ea0
Tanguy Fautre <tanguy@fautre.com>
2020-03-13 11:10:55 +0000

    Use the vcpkg CMake toolchain whenever possible. Still not in a perfect state though.

--- build_windows.bat
+++ build_windows.bat
@@ -1,7 +1,7 @@
 cd build || goto :error
 mkdir windows || goto :error
 cd windows || goto :error
-cmake -D CMAKE_PREFIX_PATH=../build/vcpkg.windows/installed/x64-windows-static/ -G "Visual Studio 16 2019" -A "x64" ../.. || goto :error
+cmake -D VCPKG_TARGET_TRIPLET=x64-windows-static -D CMAKE_TOOLCHAIN_FILE=../vcpkg.windows/scripts/buildsystems/vcpkg.cmake -G "Visual Studio 16 2019" -A "x64" ../.. || goto :error
 msbuild ParquetSharp.sln /t:restore || goto :error
 msbuild ParquetSharp.sln /t:Rebuild /p:Configuration=Release || goto :error
 cd ..
