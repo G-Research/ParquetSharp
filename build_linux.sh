b8c972574adb19706cf1b45ca41d3f8febd3122f
Tanguy Fautre <GPSnoopy@users.noreply.github.com>
2020-03-13 13:38:26 +0000

    Use the vcpkg CMake toolchain whenever possible. Still not in a perfect state though. (#115)

--- build_linux.sh
+++ build_linux.sh
@@ -3,5 +3,5 @@ set -e
 
 mkdir --parents build/linux
 cd build/linux
-cmake -D CMAKE_PREFIX_PATH=../build/vcpkg.linux/installed/x64-linux/ ../..
+cmake -D VCPKG_TARGET_TRIPLET=x64-linux -D CMAKE_TOOLCHAIN_FILE=../vcpkg.linux/scripts/buildsystems/vcpkg.cmake ../..
 make -j
