82db92d8e59a4d092ae3fc0f4a7d59b48a106f45
Tanguy Fautre <GPSnoopy@users.noreply.github.com>
2020-04-23 16:46:37 +0100

    Use GitHub Actions cache to avoid recompiling vcpkg dependencies every time. (#124)
    * Consolidate vcpkg tag/commit-id in vcpkg_version.txt.
    Use GitHub Actions cache to avoid recompiling vcpkg dependencies every time.
    
    * GitHub Actions cache requires a directory path, not a file.
    
    * Use raw export, GitHub Actions cache uses tar.gz under the hood.
    Bump the cache key to avoid conflict with previous attempts.
    
    * Change wording.

--- vcpkg_windows.bat
+++ vcpkg_windows.bat
@@ -1,8 +1,9 @@
+set /p VCPKG_VERSION=<vcpkg_version.txt || goto :error
 mkdir build
 cd build || goto :error
 git clone https://github.com/microsoft/vcpkg.git vcpkg.windows || goto :error
 cd vcpkg.windows || goto :error
-git checkout master || goto :error
+git checkout %VCPKG_VERSION% || goto :error
 call bootstrap-vcpkg.bat || goto :error
 
 vcpkg.exe install arrow:x64-windows-static || goto :error
