57dfe22fe687a56fad7708700439d4162ff4db74
Jonathan Giannuzzi <jgiannuzzi@users.noreply.github.com>
2020-04-24 22:27:06 +0200

    Fix broken vcpkg build (#130)
    * Fix typo in CI Windows vcpkg build
    
    * Fix issue in CI Linux vcpkg build
    
    boost-thread does not build with release only (and maybe others)

--- vcpkg_linux.sh
+++ vcpkg_linux.sh
@@ -7,7 +7,6 @@ mkdir -p build
 cd build
 git clone $vcpkg_url -b $vcpkg_ref vcpkg.linux
 cd vcpkg.linux
-[ "$GITHUB_ACTIONS" == "true" ] && echo "set(VCPKG_BUILD_TYPE release)" >> triplets/x64-linux.cmake
 ./bootstrap-vcpkg.sh
 
 ./vcpkg install arrow:x64-linux
