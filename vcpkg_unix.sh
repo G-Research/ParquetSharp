2629a365ae385b90da928a2252f2008532bec283
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2020-04-26 13:00:11 +0200

    Merge Linux and macOS shell scripts

---
+++ vcpkg_unix.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+case $(uname) in
+  Linux)
+    os=linux
+    triplet=x64-linux
+    ;;
+  Darwin)
+    os=macos
+    triplet=x64-osx
+    if ! which brew >/dev/null || [ ! -x $(brew --prefix)/opt/bison/bin/bison ]
+    then
+      echo 'The version of bison provided with macOS is too old.'
+      echo 'Please install a newer version with Homebrew (https://brew.sh):'
+      echo '$ brew install bison'
+      exit 1
+    else
+      export PATH="$(brew --prefix)/opt/bison/bin:$PATH"
+    fi
+    ;;
+  *)
+    echo "OS not supported"
+    exit 1
+    ;;
+esac
+
+read -r vcpkg_url vcpkg_ref < vcpkg_version.txt
+
+mkdir -p build
+cd build
+git clone $vcpkg_url -b $vcpkg_ref vcpkg.$os
+cd vcpkg.$os
+./bootstrap-vcpkg.sh
+
+./vcpkg install arrow:$triplet
