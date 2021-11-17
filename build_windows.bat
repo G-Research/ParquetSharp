a410a261931bedfa30358c6fb0f4786da3c6df7f
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2021-11-17 18:58:57 +0100

    Update Windows build scripts to align with Unix

--- build_windows.bat
+++ build_windows.bat
@@ -1,10 +1,6 @@
-cd build || goto :error
-mkdir Windows || goto :error
-cd Windows || goto :error
-cmake -D VCPKG_TARGET_TRIPLET=x64-windows-static -D CMAKE_TOOLCHAIN_FILE=../vcpkg.Windows/scripts/buildsystems/vcpkg.cmake -G "Visual Studio 16 2019" -A "x64" ../.. || goto :error
-msbuild ParquetSharp.sln -t:ParquetSharpNative:Rebuild -p:Configuration=Release || goto :error
-cd ..
-cd ..
+set triplet=x64-windows-static
+cmake -B build/%triplet% -S . -D VCPKG_TARGET_TRIPLET=%triplet% -D CMAKE_TOOLCHAIN_FILE=../vcpkg.%triplet%/scripts/buildsystems/vcpkg.cmake -G "Visual Studio 16 2019" -A "x64" || goto :error
+msbuild build/%triplet%/ParquetSharp.sln -t:ParquetSharpNative:Rebuild -p:Configuration=Release || goto :error
 
 exit /b
 
