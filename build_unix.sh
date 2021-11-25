50580901b67242aaac67656dfbf1235b20986c8f
Jonathan Giannuzzi <jgiannuzzi@users.noreply.github.com>
2021-11-25 14:34:52 +0000

    Enable support for arm64 (#231)
    * Use a version of vcpkg that has arm64 arrow
    
    * Add support for arm64 in build scripts
    
    * Enable x64 assemblies only for .NET Framework
    
    This allows the .NET Standard/Core/5.0 assemblies
    to work with both x64 and arm64.
    
    * Update Unix build script for cross-compilation
    
    * Allow building multiple architectures of ParquetSharp on the same host
    
    * Allow cross-compilation on Unix for vcpkg
    
    * Only build release configuration in CI on Unix
    
    * Update Windows build scripts to align with Unix
    
    * Fix native lib package copy
    
    * CI workflow formatting
    
    * Add missing names to CI jobs
    
    * Update CI pipeline to build for arm64 as well
    
    * Simplify local nuget source usage
    
    * Run tests on linux arm64 via Docker+QEMU

--- build_unix.sh
+++ build_unix.sh
@@ -1,14 +1,34 @@
 #!/bin/sh
 set -e
 
+case ${1:-$(uname -m)} in
+  x86_64|x64)
+    vcpkg_arch=x64
+    linux_arch=x86_64
+    osx_arch=x86_64
+    ;;
+  aarch64|arm64)
+    vcpkg_arch=arm64
+    linux_arch=aarch64
+    osx_arch=arm64
+    ;;
+  *)
+    echo "Architecture not supported"
+    exit 1
+    ;;
+esac
+
 case $(uname) in
   Linux)
-    os=Linux
-    triplet=x64-linux
+    os=linux
+    options="-D CMAKE_SYSTEM_PROCESSOR=$linux_arch \
+             -D CMAKE_C_COMPILER=$(which $linux_arch-linux-gnu-gcc) \
+             -D CMAKE_CXX_COMPILER=$(which $linux_arch-linux-gnu-g++) \
+             -D CMAKE_STRIP=$(which $linux_arch-linux-gnu-strip)"
     ;;
   Darwin)
-    os=macOS
-    triplet=x64-osx
+    os=osx
+    options="-D CMAKE_OSX_ARCHITECTURES=$osx_arch"
     ;;
   *)
     echo "OS not supported"
@@ -16,7 +36,7 @@ case $(uname) in
     ;;
 esac
 
-mkdir -p build/$os
-cd build/$os
-cmake -D VCPKG_TARGET_TRIPLET=$triplet -D CMAKE_TOOLCHAIN_FILE=../vcpkg.$os/scripts/buildsystems/vcpkg.cmake ../..
-make -j
+triplet=$vcpkg_arch-$os
+
+cmake -B build/$triplet -S . -D VCPKG_TARGET_TRIPLET=$triplet -D CMAKE_TOOLCHAIN_FILE=../vcpkg.$triplet/scripts/buildsystems/vcpkg.cmake $options
+cmake --build build/$triplet -j
--- vcpkg_windows.bat
+++ vcpkg_windows.bat
@@ -3,16 +3,18 @@ for /f "tokens=1,2" %%a in (vcpkg_version.txt) do (
   set vcpkg_ref=%%b
 ) || goto :error
 
+set triplet=x64-windows-static
+
 mkdir build
 cd build || goto :error
 rem Clone without checking out a branch, as vcpkg_ref could be a commit SHA
-git clone %vcpkg_url% vcpkg.Windows || goto :error
-cd vcpkg.Windows || goto :error
+git clone %vcpkg_url% vcpkg.%triplet% || goto :error
+cd vcpkg.%triplet% || goto :error
 git checkout %vcpkg_ref% || goto :error
-if "%GITHUB_ACTIONS%"=="true" echo set(VCPKG_BUILD_TYPE release) >> triplets\x64-windows-static.cmake || goto :error
+if "%GITHUB_ACTIONS%"=="true" echo set(VCPKG_BUILD_TYPE release) >> triplets\%triplet%.cmake || goto :error
 call .\bootstrap-vcpkg.bat || goto :error
 
-.\vcpkg.exe install arrow:x64-windows-static || goto :error
+.\vcpkg.exe install arrow:%triplet% || goto :error
 
 cd ..
 cd ..
