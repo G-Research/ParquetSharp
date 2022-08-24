fd7aa403e12e7b9524d8afa85bfe1559e0c0babf
Dan Smith <dan@colectica.com>
2022-08-24 11:24:21 -0500

    fix code formatting issue in InternalsVisibleTo

--- csharp/InternalsVisibleTo.cs
+++ csharp/InternalsVisibleTo.cs
@@ -1,12 +1,12 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("ParquetSharp.Test, PublicKey=" + 
-  "002400000480000094000000060200000024000" +
-  "0525341310004000001000100a13d5846489ccb" +
-  "673c205369a5a9d65d406d44884e2e76ccdf25c" +
-  "0de45d07154a6bfe77fcb015847af54c1498b78" +
-  "36961196252bd14e446219306f603ad88f6866d" +
-  "b6e687871d5412db3463b387584f4f1c5479d55" +
-  "3c43791d29fa28dbd232fea78bff3333c853868" +
-  "4620d8afe1c7974eb37a2cb38303c95aa2b3ceb" +
-  "9b64c5dd")]
+[assembly: InternalsVisibleTo("ParquetSharp.Test, PublicKey=" +
+                              "002400000480000094000000060200000024000" +
+                              "0525341310004000001000100a13d5846489ccb" +
+                              "673c205369a5a9d65d406d44884e2e76ccdf25c" +
+                              "0de45d07154a6bfe77fcb015847af54c1498b78" +
+                              "36961196252bd14e446219306f603ad88f6866d" +
+                              "b6e687871d5412db3463b387584f4f1c5479d55" +
+                              "3c43791d29fa28dbd232fea78bff3333c853868" +
+                              "4620d8afe1c7974eb37a2cb38303c95aa2b3ceb" +
+                              "9b64c5dd")]
