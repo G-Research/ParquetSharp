94590904038f3283a624bf50c2b90c58c244259d
Juan Escalada <juanescalada175@gmail.com>
2023-11-20 19:25:25 +0000

    Remove redundant constructors

--- csharp/RowOriented/ParquetRowWriter.cs
+++ csharp/RowOriented/ParquetRowWriter.cs
@@ -13,45 +13,6 @@ namespace ParquetSharp.RowOriented
     {
         internal delegate void WriteAction(ParquetRowWriter<TTuple> parquetRowWriter, TTuple[] rows, int length);
 
-        internal ParquetRowWriter(
-            string path,
-            Column[] columns,
-            Compression compression,
-            IReadOnlyDictionary<string, string>? keyValueMetadata,
-            WriteAction writeAction)
-            : this(new ParquetFileWriter(path, columns, compression, keyValueMetadata), writeAction)
-        {
-        }
-
-        internal ParquetRowWriter(
-            string path,
-            Column[] columns,
-            WriterProperties writerProperties,
-            IReadOnlyDictionary<string, string>? keyValueMetadata,
-            WriteAction writeAction)
-            : this(new ParquetFileWriter(path, columns, writerProperties, keyValueMetadata), writeAction)
-        {
-        }
-
-        internal ParquetRowWriter(
-            OutputStream outputStream,
-            Column[] columns,
-            Compression compression,
-            IReadOnlyDictionary<string, string>? keyValueMetadata,
-            WriteAction writeAction)
-            : this(new ParquetFileWriter(outputStream, columns, compression, keyValueMetadata), writeAction)
-        {
-        }
-
-        internal ParquetRowWriter(
-            OutputStream outputStream,
-            Column[] columns,
-            WriterProperties writerProperties,
-            IReadOnlyDictionary<string, string>? keyValueMetadata,
-            WriteAction writeAction)
-            : this(new ParquetFileWriter(outputStream, columns, writerProperties, keyValueMetadata), writeAction)
-        {
-        }
 
         internal ParquetRowWriter(
             string path,
