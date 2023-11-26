b603a37d3a13b42a4247b4cc0f27a1f22c8a0a5f
Adam Reeve <adreeve@gmail.com>
2023-11-27 12:00:38 +1300

    Add test to reproduce crash when using invalid column writer

--- csharp.test/TestLogicalColumnWriter.cs
+++ csharp.test/TestLogicalColumnWriter.cs
@@ -27,5 +27,27 @@ namespace ParquetSharp.Test
 
             writer.Close();
         }
+
+        [Test]
+        public static void TestWriteAfterNextColumn()
+        {
+            var schemaColumns = new Column[]
+            {
+                new Column<int>("A"),
+                new Column<float>("B"),
+            };
+
+            using var buffer = new ResizableBuffer();
+            using var outStream = new BufferOutputStream(buffer);
+            using var writer = new ParquetFileWriter(outStream, schemaColumns);
+            using var rowGroupWriter = writer.AppendRowGroup();
+
+            using var colWriterA = rowGroupWriter.NextColumn().LogicalWriter<int>();
+            using var colWriterB = rowGroupWriter.NextColumn().LogicalWriter<float>();
+
+            colWriterA.WriteBatch(new int[] {0, 1, 2, 3, 4});
+
+            writer.Close();
+        }
     }
 }
