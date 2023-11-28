a1881eb7aa19f75914661de97b4948900580c15c
Adam Reeve <adreeve@gmail.com>
2023-11-29 12:23:05 +1300

    Refactor skip test to use test cases for different column types

--- csharp.test/TestLogicalColumnReader.cs
+++ csharp.test/TestLogicalColumnReader.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using ParquetSharp.IO;
 using NUnit.Framework;
 
@@ -40,12 +42,15 @@ namespace ParquetSharp.Test
                 "but the actual element type is 'System.Nullable`1[System.Int32]'."));
         }
 
-        [Test]
-        public static void TestSkip()
+        [TestCaseSource(nameof(TestCases))]
+        public static void TestSkip(TestCase testCase)
         {
-            const int numRows = 10;
-            var schemaColumns = new Column[] {new Column<int?>("col")};
-            var values = Enumerable.Range(0, numRows).Select(val => (int?) val).ToArray();
+            const int numRows = 100;
+
+            var schemaColumns = new Column[]
+            {
+                testCase.ColumnDefinition
+            };
 
             using var buffer = new ResizableBuffer();
 
@@ -53,71 +58,107 @@ namespace ParquetSharp.Test
             {
                 using var writer = new ParquetFileWriter(outStream, schemaColumns);
                 using var rowGroupWriter = writer.AppendRowGroup();
-                using var colWriter = rowGroupWriter.NextColumn().LogicalWriter<int?>();
-
-                colWriter.WriteBatch(values);
-
+                using var colWriter = rowGroupWriter.NextColumn();
+                testCase.WriteColumn(colWriter, numRows);
                 writer.Close();
             }
 
             using var inStream = new BufferReader(buffer);
             using var fileReader = new ParquetFileReader(inStream);
             using var rowGroupReader = fileReader.RowGroup(0);
-            using var column = rowGroupReader.Column(0);
-            using var logicalColumnReader = column.LogicalReader<int?>();
+            using var logicalColumnReader = rowGroupReader.Column(0).LogicalReader(useNesting: true);
 
-            const int numToSkip = 5;
+            // skip first 25 rows
+            var skipped = logicalColumnReader.Skip(25);
+            Assert.That(skipped, Is.EqualTo(25));
 
-            var skipped = logicalColumnReader.Skip(numToSkip);
+            // read 25 rows
+            testCase.VerifyRead(logicalColumnReader, 25, 25);
 
-            Assert.AreEqual(numToSkip, skipped);
+            // skip another 25 rows
+            skipped = logicalColumnReader.Skip(25);
+            Assert.That(skipped, Is.EqualTo(25));
+
+            // read the last 25 rows
+            testCase.VerifyRead(logicalColumnReader, 75, 25);
         }
 
-        [Test]
-        public static void SkipIntArrayColumn()
+        public abstract class TestCase
         {
-            var idColumn = new Column<int>("id");
-            var arrayColumn = new Column<int[]>("array_col");
-
-            var idValues = new int[] {1, 2, 3};
-            var arrayValues = new int[][]
+            protected TestCase(string name, Column definition)
             {
-                new int[] {10, 20},
-                new int[] {30},
-                new int[] {40, 50, 60}
-            };
-
-            var buffer = new ResizableBuffer();
-            {
-                using var outStream = new BufferOutputStream(buffer);
-                using var writer = new ParquetFileWriter(outStream, new Column[] {idColumn, arrayColumn});
-                using (var rowGroupWriter = writer.AppendRowGroup())
-                {
-                    using (var idWriter = rowGroupWriter.NextColumn().LogicalWriter<int>())
-                    {
-                        idWriter.WriteBatch(idValues);
-                    }
-
-                    using var arrayWriter = rowGroupWriter.NextColumn().LogicalWriter<int[]>();
-                    foreach (var array in arrayValues)
-                    {
-                        arrayWriter.WriteBatch(new[] {array});
-                    }
-                }
-                writer.Close();
+                _name = name;
+                ColumnDefinition = definition;
             }
 
-            using var inStream = new BufferReader(buffer);
-            using var reader = new ParquetFileReader(inStream);
-            using var rowGroupReader = reader.RowGroup(0);
+            public override string ToString() => _name;
+            public Column ColumnDefinition { get; }
+            public abstract void WriteColumn(ColumnWriter writer, int numRows);
+            public abstract void VerifyRead(LogicalColumnReader reader, int offset, int length);
+            private readonly string _name;
+        }
 
-            using var arrayReader = rowGroupReader.Column(1).LogicalReader<int[]>();
-            arrayReader.Skip(1);
+        public static IEnumerable<TestCase> TestCases()
+        {
+            foreach (var type in typeof(TestLogicalColumnReader).GetTypeInfo().DeclaredNestedTypes)
+            {
+                if (type == typeof(TestCase))
+                {
+                    continue;
+                }
+                if (type.IsAssignableTo(typeof(TestCase)))
+                {
+                    yield return (TestCase) Activator.CreateInstance(type)!;
+                }
+            }
+        }
 
-            var readArray = new int[2][];
-            arrayReader.ReadBatch(readArray, 0, 2);
+        /// <summary>
+        /// Non-nullable integer data that uses a DirectReader
+        /// </summary>
+        public class IntTestCase : TestCase
+        {
+            public IntTestCase() : base("int column", new Column<int>("x")) {}
 
-            Assert.AreEqual(arrayValues.Skip(1).ToArray(), readArray);
+            public override void WriteColumn(ColumnWriter writer, int numRows)
+            {
+                _expectedData = Enumerable.Range(0, numRows).ToArray();
+                var logicalWriter = writer.LogicalWriter<int>();
+                logicalWriter.WriteBatch(_expectedData);
+            }
+
+            public override void VerifyRead(LogicalColumnReader reader, int offset, int length)
+            {
+                var readResult = ((LogicalColumnReader<int>) reader).ReadAll(length);
+                Assert.That(readResult, Is.EqualTo(_expectedData!.Skip(offset).Take(length).ToArray()));
+            }
+
+            private int[]? _expectedData;
+        }
+
+        /// <summary>
+        /// Integer array valued column data that uses an ArrayReader and array values are read directly from the buffered reader
+        /// </summary>
+        public class IntArrayTestCase : TestCase
+        {
+            public IntArrayTestCase() : base("int array column", new Column<int[]>("x")) {}
+
+            public override void WriteColumn(ColumnWriter writer, int numRows)
+            {
+                _expectedData = Enumerable.Range(0, numRows)
+                    .Select(row => Enumerable.Range(row, row % 5).ToArray())
+                    .ToArray();
+                var logicalWriter = writer.LogicalWriter<int[]>();
+                logicalWriter.WriteBatch(_expectedData);
+            }
+
+            public override void VerifyRead(LogicalColumnReader reader, int offset, int length)
+            {
+                var readResult = ((LogicalColumnReader<int[]>) reader).ReadAll(length);
+                Assert.That(readResult, Is.EqualTo(_expectedData!.Skip(offset).Take(length).ToArray()));
+            }
+
+            private int[][]? _expectedData;
         }
     }
 }
