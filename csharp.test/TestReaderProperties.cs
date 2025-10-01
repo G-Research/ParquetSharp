d25ccf61203a96e6f6ac845e953076e9cb6f5712
Hai-Anh (Hennessy) Nguyen <126405175+haianhng31@users.noreply.github.com>
2025-10-01 01:08:45 -0700

    Add Thrift string size limit reader property (#564)

--- csharp.test/TestReaderProperties.cs
+++ csharp.test/TestReaderProperties.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using ParquetSharp.IO;
 
 namespace ParquetSharp.Test
 {
@@ -35,6 +36,45 @@ namespace ParquetSharp.Test
             Assert.That(p.IsBufferedStreamEnabled, Is.True);
             p.DisableBufferedStream();
             Assert.That(p.IsBufferedStreamEnabled, Is.False);
+
+            p.SetThriftStringSizeLimit(2048576);
+            Assert.That(p.ThriftStringSizeLimit, Is.EqualTo(2048576));
+        }
+
+        [Test]
+        public static void TestSetThriftStringSizeLimit_ReturnException()
+        {
+            using var buffer = new ResizableBuffer();
+
+            using (var output = new BufferOutputStream(buffer))
+            {
+                var longColumnName = new string('X', 100); // 100 chars
+                var schema = new Column[] { new Column<string>(longColumnName) };
+
+                using var writer = new ParquetFileWriter(output, schema);
+                using (var rowGroup = writer.AppendRowGroup())
+                {
+                    using var colWriter = rowGroup.NextColumn().LogicalWriter<string>();
+                    colWriter.WriteBatch(new[] { "hello" });
+                }
+                writer.Close();
+            }
+
+            // Configure reader with a small thrift string size limit
+            using var props = ReaderProperties.GetDefaultReaderProperties();
+            props.SetThriftStringSizeLimit(10);
+
+            var ex = Assert.Throws<ParquetException>(() =>
+            {
+                using var input = new BufferReader(buffer);
+                using var reader = new ParquetFileReader(input, props);
+                var rg = reader.RowGroup(0); // Force metadata read
+            });
+
+            // Validate the exception is related to the thrift string size limit
+            Assert.That(ex?.Message,
+                Does.Contain("Couldn't deserialize thrift: TProtocolException: Exceeded size limit")
+                    .IgnoreCase);
         }
 
         [TestCaseSource(typeof(MemoryPools), nameof(MemoryPools.NonNullTestCases))]
