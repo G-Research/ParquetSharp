9bad8f87242f4ba3e487d63fe4232d02c6192dab
larrytamnjong <larrytabeh@gmail.com>
2026-03-04 13:30:02 +0100

    Add ParquetSharp config benchmarks project

---
+++ csharp.config.benchmarks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ParquetSharp.Config.Benchmarks
+{
+    internal static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await ParquetSharpConfigBenchmarks.RunAsync(args);
+        }
+    }
+}
