8b7bf116ce99045df53b543df8f9118cb523b489
Marcin Krystianc <marcin.krystianc@gmail.com>
2026-03-06 13:11:37 +0100

    no ci specific

--- build_windows.ps1
+++ build_windows.ps1
@@ -42,38 +42,12 @@ $triplet = "$arch-windows-static"
 $build_types = @("Debug", "Release")
 
 $options = @()
-if ($Env:GITHUB_ACTIONS -eq "true") {
-  $build_types = @("Release")
-  $customTripletsDir = "$(Get-Location)/build/custom-triplets"
-  New-Item -Path $customTripletsDir -ItemType "directory" -Force > $null
-  foreach ($subdir in @("", "community")) {
-    $sourceTripletFile = "$vcpkgDir/triplets/$subdir/$triplet.cmake"
-    if (Test-Path $sourceTripletFile) {
-      $customTripletFile = "$customTripletsDir/$triplet.cmake"
-      Copy-Item -Path $sourceTripletFile -Destination $customTripletFile
-      Add-Content -Path $customTripletFile -Value "set(VCPKG_BUILD_TYPE release)"
-
-      # Ensure vcpkg uses the same MSVC version to build dependencies as we use to build the ParquetSharp library.
-      # By default, vcpkg uses the most recent version it can find, which might not be the same as what msbuild uses.
-      $vsInstPath = & "${env:ProgramFiles(x86)}/Microsoft Visual Studio/Installer/vswhere.exe" -latest -property installationPath
-      Import-Module "$vsInstPath/Common7/Tools/Microsoft.VisualStudio.DevShell.dll"
-      Enter-VsDevShell -VsInstallPath $vsInstPath -SkipAutomaticLocation
-      $clPath = Get-Command cl.exe | Select -ExpandProperty "Source"
-      $toolsetVersion = $clPath.Split("\")[8]
-      if (-not $toolsetVersion.StartsWith("14.")) { throw "Couldn't get toolset version from path '$clPath'" }
-      Write-Output "Using platform toolset version = $toolsetVersion"
-      Add-Content -Path $customTripletFile -Value "set(VCPKG_PLATFORM_TOOLSET_VERSION $toolsetVersion)"
-    }
-  }
-  $options += "-DVCPKG_OVERLAY_TRIPLETS=$customTripletsDir"
-}
-
 $options += "-DCMAKE_VERBOSE_MAKEFILE=ON"
 
 cmake -B build/$triplet -S . -D VCPKG_TARGET_TRIPLET=$triplet -D CMAKE_TOOLCHAIN_FILE=$vcpkgDir/scripts/buildsystems/vcpkg.cmake -G "Visual Studio 17 2022" -A $arch @options
 if (-not $?) { throw "cmake failed" }
 
 foreach ($build_type in $build_types) {
-  msbuild build/$triplet/ParquetSharp.sln -t:ParquetSharpNative:Rebuild -p:Configuration=$build_type
-  if (-not $?) { throw "msbuild failed" }
+  cmake --build build/$triplet --target ParquetSharpNative --config $build_type
+  if (-not $?) { throw "cmake build failed" }
 }
