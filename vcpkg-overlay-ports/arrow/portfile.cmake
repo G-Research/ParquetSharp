6f605f526e94fee890645b10c95d5fe263b4f9de
Marcin Krystianc <marcin.krystianc@gmail.com>
2026-03-17 15:34:32 +0100

    vcpkg_on patch

--- vcpkg-overlay-ports/arrow/portfile.cmake
+++ vcpkg-overlay-ports/arrow/portfile.cmake
@@ -14,6 +14,7 @@ vcpkg_extract_source_archive(
         0004-android-datetime.patch
         0005-cmake-msvcruntime.patch
         0006-pcg-msvc-arm64.patch
+        0008-tmp.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
