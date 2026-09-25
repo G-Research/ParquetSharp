2ffe0344ec9862ee47c6329b0f9c5b4aac7f84f9
Marcin Krystianc <marcin.krystianc@gmail.com>
2026-09-25 17:51:11 +0200

    dynamic visual studio generator

--- build_windows.ps1
+++ build_windows.ps1
@@ -31,37 +31,43 @@ $triplet = "$arch-windows-static"
 
 $build_types = @("Debug", "Release")
 
-$options = @()
-if ($Env:GITHUB_ACTIONS -eq "true") {
-  $build_types = @("Release")
-  $customTripletsDir = "$(Get-Location)/build/custom-triplets"
-  New-Item -Path $customTripletsDir -ItemType "directory" -Force > $null
-  foreach ($subdir in @("", "community")) {
-    $sourceTripletFile = "$vcpkgDir/triplets/$subdir/$triplet.cmake"
-    if (Test-Path $sourceTripletFile) {
-      $customTripletFile = "$customTripletsDir/$triplet.cmake"
-      Copy-Item -Path $sourceTripletFile -Destination $customTripletFile
-      Add-Content -Path $customTripletFile -Value "set(VCPKG_BUILD_TYPE release)"
-
-      # Ensure vcpkg uses the same MSVC version to build dependencies as we use to build the ParquetSharp library.
-      # By default, vcpkg uses the most recent version it can find, which might not be the same as what msbuild uses.
-      $vsInstPath = & "${env:ProgramFiles(x86)}/Microsoft Visual Studio/Installer/vswhere.exe" -latest -property installationPath
-      Import-Module "$vsInstPath/Common7/Tools/Microsoft.VisualStudio.DevShell.dll"
-      Enter-VsDevShell -VsInstallPath $vsInstPath -SkipAutomaticLocation
-      $clPath = Get-Command cl.exe | Select -ExpandProperty "Source"
-      $toolsetVersion = $clPath.Split("\")[8]
-      if (-not $toolsetVersion.StartsWith("14.")) { throw "Couldn't get toolset version from path '$clPath'" }
-      Write-Output "Using platform toolset version = $toolsetVersion"
-      Add-Content -Path $customTripletFile -Value "set(VCPKG_PLATFORM_TOOLSET_VERSION $toolsetVersion)"
-    }
-  }
-  $options += "-DVCPKG_OVERLAY_TRIPLETS=$customTripletsDir"
+# Ensure vcpkg uses the same MSVC version to build dependencies as we use to build the ParquetSharp library.
+# By default, vcpkg uses the most recent version it can find, which might not be the same as what msbuild uses.
+if ($null -eq $Env:VCToolsVersion) {
+  $vsInstPath = & "${env:ProgramFiles(x86)}/Microsoft Visual Studio/Installer/vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
+  Import-Module "$vsInstPath/Common7/Tools/Microsoft.VisualStudio.DevShell.dll"
+  Enter-VsDevShell -VsInstallPath $vsInstPath -SkipAutomaticLocation
+}
+$toolsetVersion = $Env:VCToolsVersion
+Write-Output "Using platform toolset version = $toolsetVersion"
+$generator = switch ($Env:VisualStudioVersion) {
+  "17.0" { "Visual Studio 17 2022" }
+  "18.0" { "Visual Studio 18 2026" }
+  default { throw "Unsupported Visual Studio version: $Env:VisualStudioVersion" }
 }
 
-cmake -B build/$triplet -S . -D VCPKG_TARGET_TRIPLET=$triplet -D CMAKE_TOOLCHAIN_FILE=$vcpkgDir/scripts/buildsystems/vcpkg.cmake -G "Visual Studio 17 2022" -A $arch @options
+if ($Env:GITHUB_ACTIONS -eq "true") {
+  $build_types = @("Release")
+}
+
+$customTripletsDir = "$(Get-Location)/build/custom-triplets"
+New-Item -Path $customTripletsDir -ItemType "directory" -Force > $null
+foreach ($subdir in @("", "community")) {
+  $sourceTripletFile = "$vcpkgDir/triplets/$subdir/$triplet.cmake"
+  if (Test-Path $sourceTripletFile) {
+    $customTripletFile = "$customTripletsDir/$triplet.cmake"
+    Copy-Item -Path $sourceTripletFile -Destination $customTripletFile
+    if ($Env:GITHUB_ACTIONS -eq "true") {
+      Add-Content -Path $customTripletFile -Value "set(VCPKG_BUILD_TYPE release)"
+    }
+    Add-Content -Path $customTripletFile -Value "set(VCPKG_PLATFORM_TOOLSET_VERSION $toolsetVersion)"
+  }
+}
+
+cmake -B build/$triplet -S . -D VCPKG_TARGET_TRIPLET=$triplet -D VCPKG_OVERLAY_TRIPLETS=$customTripletsDir -D CMAKE_TOOLCHAIN_FILE=$vcpkgDir/scripts/buildsystems/vcpkg.cmake -G $generator -D CMAKE_GENERATOR_INSTANCE=$($Env:VSINSTALLDIR.TrimEnd("\")) -A $arch -T version=$toolsetVersion
 if (-not $?) { throw "cmake failed" }
 
 foreach ($build_type in $build_types) {
-  msbuild build/$triplet/ParquetSharp.sln -t:ParquetSharpNative:Rebuild -p:Configuration=$build_type
-  if (-not $?) { throw "msbuild failed" }
+  cmake --build build/$triplet --target ParquetSharpNative --config $build_type
+  if (-not $?) { throw "cmake build failed" }
 }
